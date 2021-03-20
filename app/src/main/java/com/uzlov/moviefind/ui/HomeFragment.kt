f2d45f8b0742f47579acfdced17f0dba37ba1f46
Artem <artemisuzl@gmail.com>
2021-03-20 13:31:45 +0300

    Refactor names

--- app/src/main/java/com/uzlov/moviefind/ui/HomeFragment.kt
+++ app/src/main/java/com/uzlov/moviefind/ui/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.uzlov.moviefind
+package com.uzlov.moviefind.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.uzlov.moviefind.R
 
 class FirstFragment : Fragment() {
 
