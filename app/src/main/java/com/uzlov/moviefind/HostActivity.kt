f7512c929aa66ec9ca9f9cfbe6a46a92923cebd6
Artem <artemisuzl@gmail.com>
2021-03-20 14:13:11 +0300

    Refactoring, add button

--- app/src/main/java/com/uzlov/moviefind/HostActivity.kt
+++ app/src/main/java/com/uzlov/moviefind/HostActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.uzlov.moviefind.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment.newInstance())
+            .commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
