82d42ba868431b5e25dbb4b0fd3f24280d9b0532
Artem <artemisuzl@gmail.com>
2021-03-20 13:07:10 +0300

    Initial commit

---
+++ app/src/main/java/com/uzlov/moviefind/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.uzlov.moviefind
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
