d386727a31f2b59763ce12c79cc3699d33b3f5e1
Artem <artemisuzl@gmail.com>
2021-03-24 00:07:14 +0300

    add simple ViewModel

---
+++ app/src/main/java/com/uzlov/moviefind/model/TestFilm.kt
@@ -0,0 +1,3 @@
+package com.uzlov.moviefind.model
+
+data class TestFilm(val name:String, val genre:String)
---
+++ app/src/main/java/com/uzlov/moviefind/viewmodels/FilmsViewModel.kt
@@ -0,0 +1,29 @@
+package com.uzlov.moviefind.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.uzlov.moviefind.model.TestFilm
+import java.lang.Thread.sleep
+
+class FilmsViewModel(private val liveDataToObserve: MutableLiveData<List<TestFilm>> = MutableLiveData()) : ViewModel() {
+
+    fun getPopularFilms() : LiveData<List<TestFilm>>{
+        getLocalData()
+        return liveDataToObserve
+    }
+
+    private fun getLocalData(){
+        Thread {
+            sleep(2000)
+            liveDataToObserve.postValue(listOf(
+                    TestFilm("Бойцовский клуб","Боевик"),
+                    TestFilm("Пила 2","Ужасы"),
+                    TestFilm("Большой куш","Боевик"),
+                    TestFilm("1+1","Комедия, драма"),
+                    TestFilm("Терминатор","Боевик")
+            ))
+        }.start()
+
+    }
+}
