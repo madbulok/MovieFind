74fa1ba839705286b3620beeda93b5b52eb11f95
Artem <artemisuzl@gmail.com>
2021-03-27 20:27:28 +0300

    update mockup data class film

--- app/src/main/java/com/uzlov/moviefind/model/TestFilm.kt
+++ app/src/main/java/com/uzlov/moviefind/model/TestFilm.kt
@@ -1,3 +1,40 @@
 package com.uzlov.moviefind.model
 
-data class TestFilm(val name:String, val genre:String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class TestFilm(val name:String = "", val genre:String = "", val rating: Int = 0,
+                    val description: String = "", val studio: String = "", val year: String = "") : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString()  ?: "",
+        parcel.readInt(),
+        parcel.readString()  ?: "",
+        parcel.readString()  ?: "",
+        parcel.readString() ?: ""
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(genre)
+        parcel.writeInt(rating)
+        parcel.writeString(description)
+        parcel.writeString(studio)
+        parcel.writeString(year)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<TestFilm> {
+        override fun createFromParcel(parcel: Parcel): TestFilm {
+            return TestFilm(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TestFilm?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
