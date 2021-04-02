f4123ded554add6323f9971eee94a055366cdd3c
Artem <artemisuzl@gmail.com>
2021-04-02 21:56:21 +0300

    add request from API popular films and film by id

--- app/src/main/java/com/uzlov/moviefind/interfaces/Loadable.kt
+++ app/src/main/java/com/uzlov/moviefind/interfaces/Loadable.kt
@@ -1,6 +1,11 @@
 package com.uzlov.moviefind.interfaces
 
+import androidx.lifecycle.MutableLiveData
+import com.uzlov.moviefind.model.Film
+import com.uzlov.moviefind.model.PopularFilms
+
 @FunctionalInterface
-public interface Loadable<T> {
-    fun loadPopular(films: (List<T>) -> Unit)
+public interface Loadable {
+    fun loadPopular() : MutableLiveData<PopularFilms>
+    fun loadFilmById(id: Int) : MutableLiveData<Film>
 }
--- app/src/main/java/com/uzlov/moviefind/repository/RepositoryPopularImpl.kt
+++ app/src/main/java/com/uzlov/moviefind/repository/RepositoryPopularImpl.kt
@@ -1,11 +1,88 @@
 package com.uzlov.moviefind.repository
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.uzlov.moviefind.interfaces.Constants
 import com.uzlov.moviefind.interfaces.Loadable
+import com.uzlov.moviefind.model.Film
 import com.uzlov.moviefind.model.PopularFilms
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
-object RepositoryPopularImpl : Loadable<PopularFilms> {
+object RepositoryPopularImpl : Loadable {
 
-    override fun loadPopular(films: (List<PopularFilms>) -> Unit) {
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun loadPopular() : MutableLiveData<PopularFilms>{
+        val uriPopularFilms = URL("https://api.themoviedb.org/3/movie/popular?api_key=${Constants.API_KEY}&language=ru-RU")
+        val responseLiveData: MutableLiveData<PopularFilms> = MutableLiveData()
 
+        try {
+            Thread {
+                lateinit var urlConnection: HttpsURLConnection
+                try {
+                    urlConnection = (uriPopularFilms.openConnection() as HttpsURLConnection).apply {
+                        requestMethod = "GET"
+                        readTimeout = 10_000
+                    }
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+                    val popularFilms =
+                        Gson().fromJson(bufferedReader.getLines(), PopularFilms::class.java)
+                    responseLiveData.postValue(popularFilms)
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }.start()
+        } catch (e: MalformedURLException){
+            e.printStackTrace()
+        }
+        return responseLiveData
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun loadFilmById(id: Int): MutableLiveData<Film> {
+        val uriDetailFilm = URL("https://api.themoviedb.org/3/movie/$id?api_key=${Constants.API_KEY}&language=ru-RU")
+        val responseLiveData: MutableLiveData<Film> = MutableLiveData()
+
+        try {
+            Thread {
+                lateinit var urlConnection: HttpsURLConnection
+                try {
+                    urlConnection = (uriDetailFilm.openConnection() as HttpsURLConnection).apply {
+                        requestMethod = "GET"
+                        readTimeout = 10_000
+                    }
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+                    val film =
+                        Gson().fromJson(bufferedReader.getLines(), Film::class.java)
+                    responseLiveData.postValue(film)
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }.start()
+        } catch (e: MalformedURLException){
+            e.printStackTrace()
+        }
+        return responseLiveData
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun BufferedReader.getLines() : String {
+        return lines().collect(Collectors.joining("\n"))
     }
 }
