b8013b88e968cd5930b2f22ff652ed68cf345ace
Artem <artemisuzl@gmail.com>
2021-04-14 23:12:09 +0300

    fixup!API

--- app/src/main/java/com/uzlov/moviefind/repository/FilmApi.kt
+++ app/src/main/java/com/uzlov/moviefind/repository/FilmApi.kt
@@ -1,6 +1,7 @@
 package com.uzlov.moviefind.repository
 
 import com.uzlov.moviefind.interfaces.Constants
+import com.uzlov.moviefind.model.Credits
 import com.uzlov.moviefind.model.Film
 import com.uzlov.moviefind.model.PopularFilms
 import retrofit2.Call
@@ -29,4 +30,21 @@ interface FilmApi {
         @Query("api_key") apikey:String = Constants.API_KEY,
         @Query("language") language:String = "ru-RU",
     ) : Call<Film>
+
+    @GET("3/movie/{movie_id}/credits")
+    fun getCreditsMoviesById(
+        @Path("movie_id") id: Int,
+        @Query("api_key") apikey:String = Constants.API_KEY,
+        @Query("language") language:String = "ru-RU",
+    ) : Call<Credits>
+
+    @GET("3/search/movie")
+    fun searchFilmByName(
+        @Query("api_key") apikey:String = Constants.API_KEY,
+        @Query("language") language:String = "ru-RU",
+        @Query("query") name: String,
+        @Query("page") num: Int = 1,
+        @Query("include_adult") isAdult: Boolean = true
+    ) : Call<PopularFilms>
+
 }
