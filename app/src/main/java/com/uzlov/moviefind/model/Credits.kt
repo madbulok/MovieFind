f7988859c8ec628a5aa95a8663dc2137d3bd0e67
Artem <artemisuzl@gmail.com>
2021-04-14 23:01:25 +0300

    Add search movies by name and show result into HomeFragment.kt

---
+++ app/src/main/java/com/uzlov/moviefind/model/Credits.kt
@@ -0,0 +1,6 @@
+package com.uzlov.moviefind.model
+
+data class Credits(
+    val cast: List<Cast>,
+    val id: Int
+)
--- app/src/main/java/com/uzlov/moviefind/repository/RepositoryPopularImpl.kt
+++ app/src/main/java/com/uzlov/moviefind/repository/RepositoryPopularImpl.kt
@@ -1,9 +1,11 @@
 package com.uzlov.moviefind.repository
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.gson.GsonBuilder
 import com.uzlov.moviefind.interfaces.Constants
 import com.uzlov.moviefind.interfaces.Loadable
+import com.uzlov.moviefind.model.Credits
 import com.uzlov.moviefind.model.Film
 import com.uzlov.moviefind.model.PopularFilms
 import com.uzlov.moviefind.viewmodels.AppStateFilms
@@ -77,6 +79,41 @@ object RepositoryPopularImpl : Loadable {
 
             override fun onFailure(call: Call<PopularFilms>, t: Throwable) {
                 responseLiveData.value = AppStateFilms.Error(error = Throwable("Ошибка сервера ${t.message}"))
+            }
+        })
+        return responseLiveData
+    }
+
+    fun searchFilmsByName(name: String, isAdult: Boolean = false): LiveData<AppStateFilms> {
+        val responseLiveData: MutableLiveData<AppStateFilms> = MutableLiveData()
+
+        filmsApi.searchFilmByName(name = name, isAdult = isAdult).enqueue(object : Callback<PopularFilms> {
+            override fun onResponse(call: Call<PopularFilms>, response: Response<PopularFilms>) {
+                if (response.isSuccessful){
+                    responseLiveData.postValue(AppStateFilms.Success(filmsData = response.body()!!))
+                } else {
+                    responseLiveData.value = AppStateFilms.Error(error = Throwable("Ошибка сервера ${response.code()}"))
+                }
+            }
+
+            override fun onFailure(call: Call<PopularFilms>, t: Throwable) {
+                responseLiveData.value = AppStateFilms.Error(error = Throwable("Ошибка сервера ${t.message}"))
+            }
+        })
+        return responseLiveData
+    }
+
+    fun getCreditsMoviesById(id: Int): MutableLiveData<Credits> {
+        val responseLiveData: MutableLiveData<Credits> = MutableLiveData()
+
+        filmsApi.getCreditsMoviesById(id).enqueue(object : Callback<Credits>{
+            override fun onResponse(call: Call<Credits>, response: Response<Credits>) {
+                if (response.isSuccessful){
+                    responseLiveData.postValue(response.body()!!)
+                }
+            }
+
+            override fun onFailure(call: Call<Credits>, t: Throwable) {
             }
         })
         return responseLiveData
