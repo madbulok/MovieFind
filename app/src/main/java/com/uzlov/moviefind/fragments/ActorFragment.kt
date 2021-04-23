e67d455edb8a57ce4b1856f2e1ea53824f39f1e5
Artem <artemisuzl@gmail.com>
2021-04-23 12:58:02 +0300

    added the location on the map where the actor was born

---
+++ app/src/main/java/com/uzlov/moviefind/fragments/ActorFragment.kt
@@ -0,0 +1,122 @@
+package com.uzlov.moviefind.fragments
+
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.uzlov.moviefind.R
+import com.uzlov.moviefind.databinding.FragmentMapsBinding
+import com.uzlov.moviefind.repository.RepositoryRemoteImpl
+import com.uzlov.moviefind.viewmodels.FilmsViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.IOException
+
+class ActorFragment : Fragment() {
+
+    private lateinit var mMap: GoogleMap
+    private var idActor = 0
+    private var _viewBinding: FragmentMapsBinding? = null
+    private val viewBinding get() = _viewBinding!!
+    private val viewModel: FilmsViewModel by lazy {
+        ViewModelProvider(this).get(FilmsViewModel::class.java)
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        mMap = googleMap
+        startLoadingActor()
+    }
+
+    companion object {
+        fun newInstance(id: Int) : ActorFragment{
+            val fragment = ActorFragment()
+            val bundle = Bundle()
+            bundle.putInt("id_actor", id)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    private fun startLoadingActor() {
+        viewModel.getActorDescriptionsById(idActor).observe(viewLifecycleOwner, {
+            with(viewBinding){
+                tvNameActor.text = it.name
+                description.text = it.biography
+            }
+            runBlocking {
+                getPositionByAddress(it.place_of_birth)
+            }
+        })
+    }
+
+    private suspend fun getPositionByAddress(address : String){
+        GlobalScope.launch {
+            val gecoder = Geocoder(requireContext())
+            try {
+                val addresses = gecoder.getFromLocationName(address, 1)
+                if (addresses.size > 0) {
+                    goToAddress(addresses, address)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    private fun goToAddress(addresses: List<Address>, address: String) {
+        val location = LatLng(
+            addresses.first().latitude,
+            addresses.first().longitude
+        )
+        val handler = Handler(Looper.getMainLooper())
+        handler.post {
+            mMap.addMarker(MarkerOptions().position(location).title(address))
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    15f
+                )
+            )
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments.let {
+            idActor = it?.getInt("id_actor") ?: -1
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _viewBinding = FragmentMapsBinding.inflate(layoutInflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+
+        viewBinding.btnBack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+    }
+}
--- app/src/main/java/com/uzlov/moviefind/ui/ActorFilmsAdapter.kt
+++ app/src/main/java/com/uzlov/moviefind/ui/ActorFilmsAdapter.kt
@@ -4,11 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.uzlov.moviefind.R
 import com.uzlov.moviefind.databinding.ItemActorBinding
+import com.uzlov.moviefind.fragments.OpenMapListener
 import com.uzlov.moviefind.model.Cast
 
-class ActorFilmsAdapter : RecyclerView.Adapter<ActorFilmsHolder>() {
+class ActorFilmsAdapter(val callback: OpenMapListener) : RecyclerView.Adapter<ActorFilmsAdapter.ActorFilmsHolder>() {
     private val mListFilms:MutableList<Cast> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorFilmsHolder {
@@ -31,23 +31,26 @@ class ActorFilmsAdapter : RecyclerView.Adapter<ActorFilmsHolder>() {
         mListFilms.add(actor)
         notifyItemInserted(mListFilms.size-1)
     }
-}
 
-class ActorFilmsHolder(private val binding: ItemActorBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun onBind(actor : Cast){
-        with(binding){
-            Glide
-                .with(imageActor.context)
-                .load(actor.getFullImagePath())
-                .into(imageActor)
-            actorName.text = actor.original_name.replace(" ", System.getProperty("line.separator") ?: "\n")
+    inner class ActorFilmsHolder(private val binding: ItemActorBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(actor : Cast){
+            with(binding){
+                Glide
+                    .with(imageActor.context)
+                    .load(actor.getFullImagePath())
+                    .into(imageActor)
+                actorName.text = actor.original_name.replace(" ", System.getProperty("line.separator") ?: "\n")
+                root.setOnClickListener {
+                    callback.open(actor.id)
+                }
+            }
         }
-    }
 
-    fun recycle(){
-        Glide.get(binding.imageActor.context)
-            .requestManagerRetriever
-            .get(binding.imageActor.context)
-            .clear(binding.imageActor)
+        fun recycle(){
+            Glide.get(binding.imageActor.context)
+                .requestManagerRetriever
+                .get(binding.imageActor.context)
+                .clear(binding.imageActor)
+        }
     }
 }
